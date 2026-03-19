bcf76f21ac70d4ad29b8e8bb5260ce9333483629
Jvrr0109 <joao.ribeiro26@univille.br>
2026-03-19 20:25:41 -0300

    Add files via upload

---
+++ atividades/aula.py
@@ -0,0 +1,44 @@
+#print("True and True")
+#print("False and True")
+#print("1 == 1 and 2 == 1")
+#print("test" == "test")
+#print("True and 1 == 1")
+#print("1 == 1 or 2 !=1")
+#print("False and 0 != 0")
+#print("True or 1 == 1")
+#print("test" != "testing")
+#print("test" == 1)
+#print("not (True and False)")
+#print("not (True and False)")
+#print("not (1 ==1 amd 0 !=1)")
+#print("not (10 == 1 or 1000 == 1000)")
+#print("not (1 != 10 or 3 == 4)")
+#print(not ("testing" == "testing" and "Zed" == "Cool Guy"))
+#print( 1 == 1 and (not("testing" == 1 or 1 == 0)))
+#print ("chunky" == "bacon" and (not (3 == 4 or 3 == 3)))
+#print(3 == 3 and (not ("testing" == "testing" or "Python" == "Fun")))
+
+#numero = float(input("Insira um numero"))
+#if numero > 0:
+    #print("O resultado é positivo")
+#else:
+    #print("O resultado é negativo")
+
+#sal = float(input("Qual o seu salário?"))
+#val = float(input("Qual o valor desejado?"))
+#par = float (input("Quantas parcelas desejas fazer?"))
+#resultado = 30 / 100 * sal
+#parcela = val / par
+#if par < resultado:
+#    print("Empréstimo negado")
+#else:
+#    print("Empréstimo concedido")
+
+#num = float (input("Informe o numero "))
+#if num % 2 == 0:
+#    print("O número é par")
+#else:
+#    print("O número é ímpar")
+
+    
+
