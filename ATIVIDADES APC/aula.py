a1e082e5c8f5698e9b904c4fdbc869db379abb8a
Jvrr0109 <joao.ribeiro26@univille.br>
2026-03-22 18:43:54 -0300

    resolução das ativades de pythonn

--- ATIVIDADES APC/aula.py
+++ ATIVIDADES APC/aula.py
@@ -40,5 +40,90 @@
 #else:
 #    print("O número é ímpar")
 
-    
+#atividade 5
+'''
 
+    salario = float(input("Digite seu salário: "))
+
+    if salario <= 1412:
+        inss = 0.075
+    elif salario <= 2666:
+        inss = 0.09
+    elif salario <= 4000:
+        inss = 0.12
+    elif salario >= 7786:
+        inss = 0.14
+    else:
+        inss = 0.14
+    desconto = salario * inss
+    liq = salario - desconto
+
+    print(f"inss: {desconto}")
+    print(f"liquido: {liq}")
+'''
+#atividade 6
+'''
+numA = float(input("Valor A: "))
+numB = float(input("Valor B: "))
+
+if numA > numB:
+    print (f"o valor de A é maior que B: {numB}")
+elif numB > numA:
+    print (f"o valor de B é maior que A {numA}")
+else:
+    print(f"ambos os valores são iguais")
+'''
+#atividade 7
+'''
+primeiraNota = float(input("Digite sua nota: "))
+segundaNota = float(input("Digite sua nota: "))
+terceiraNota = float(input("Digite sua nota: "))
+
+soma = (primeiraNota * 2 + segundaNota *3 + terceiraNota * 5) / 10
+
+
+print ("A sua média é de: ", soma)
+'''
+
+#atividade 8
+nome = (input("qual seu nome EV:"))
+
+português = float(input( "Qual foi a sua nota em Português: "))
+matematica = float(input("Qual foi a sua nota em Matemática: "))
+conhecimentosGerais = float(input("Qual foi a sua nota Conhecimentos Gerais: "))
+
+
+# Português
+if português >= 7:
+    print("Português: Aprovado")
+else:
+    print("Português: Reprovado")
+
+# Matemática
+if matematica >= 7:
+    print("Matemática: Aprovado")
+else:
+    print("Matemática: Reprovado")
+
+# Conhecimentos Gerais
+if conhecimentosGerais >= 7:
+    print("Conhecimentos Gerais: Aprovado")
+else:
+    print("Conhecimentos Gerais: Reprovado")
+
+reprovadas = []
+if português < 7:
+    reprovadas.append("Português")
+if matematica < 7:
+    reprovadas.append("Matemática")
+if conhecimentosGerais < 7:
+    reprovadas.append("Conhecimentos Gerais")
+
+resultado = (português + matematica + conhecimentosGerais) / 3
+
+print("Sua média foi de:", resultado )
+# Mostra o resultado
+if len(reprovadas) == 0:
+    print("Parabéns! Você foi aprovado em todas as matérias.")
+else:
+    print("Você foi reprovado nas seguintes matérias:", " e ".join(reprovadas))
---
+++ ATIVIDADES APC/calculo1.py
@@ -0,0 +1,9 @@
+salBruto = 3000
+vlhr = 10
+numhrs = 200
+inss = 9
+
+desconto = salBruto * inss / 100
+salLiquido = salBruto - desconto
+
+print("valor líquido será de:", salLiquido )
